619194869380509cd4c2b9ef43eebf09b598fa8f
dimitarrusev <dimitarrusev@users.noreply.github.com>
2017-08-07 16:20:16 +0200

    chore(prerender): add prerender script

---
+++ scripts/prerender.js
@@ -0,0 +1,21 @@
+require('zone.js/dist/zone-node');
+require('reflect-metadata');
+const fs = require('fs');
+const { renderModuleFactory } = require('@angular/platform-server');
+const { AppServerModuleNgFactory } = require('../dist/server/main.bundle.js');
+
+const args = process.argv.slice(2);
+
+if (args.length != 2) {
+  process.stdout.write("Usage: node prerender.js <type> <url>");
+  process.exit();
+}
+
+renderModuleFactory(
+  AppServerModuleNgFactory,
+  {
+    document: (args[0] === 'static') ? fs.readFileSync('./src/index.html', 'utf-8') : fs.readFileSync('./dist/client/index.html', 'utf-8'),
+    url: args[1]
+  }
+).then(document => process.stdout.write(document));
+
