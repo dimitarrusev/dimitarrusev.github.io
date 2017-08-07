9574186d2d2115a9d0681d5c23aee4e4949fcecb
dimitarrusev <dimitarrusev@users.noreply.github.com>
2017-08-07 16:59:46 +0200

    feat(app-routing, blog): add app-routing module and blog component

--- src/app/app-browser.module.ts
+++ src/app/app-browser.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   imports: [
-    BrowserModule.withServerTransition({ appId: 'dimitarrusev.github.io' })
+    BrowserModule.withServerTransition({ appId: 'dimitarrusev.github.io' }),
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BlogComponent } from './blog/blog.component';
+
+const routes = [
+  { path: '', component: BlogComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
