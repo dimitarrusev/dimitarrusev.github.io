744bf8d52af86b48bd1ca1a59dc32f7a345ab487
dimitarrusev <dimitarrusev@users.noreply.github.com>
2017-10-29 13:10:37 +0100

    refactor: clean script
    [skip ci]

--- scripts/clean.js
+++ scripts/clean.js
@@ -9,21 +9,25 @@ const path = require('path');
 const shell = require('shelljs');
 const config = require('../build.conf.js');
 
+switch (process.argv[2]) {
+    case 'process':
+      const pagesDir = path.resolve(__dirname, '../', config.process.pages.dir);
+      const articlesDir = path.resolve(__dirname, '../', config.process.articles.dir);
 
-if (process.argv[2] === 'process') {
-  let pagesDir = path.resolve(__dirname, '../', config.process.pages.dir);
-  let articlesDir = path.resolve(__dirname, '../', config.process.articles.dir);
+      shell.rm('-rf', `${pagesDir}/**/*.json`);
+      shell.rm('-rf', `${articlesDir}/**/*.json`);
+      break;
 
-  shell.rm('-rf', `${pagesDir}/**/*.json`);
-  shell.rm('-rf', `${articlesDir}/**/*.json`);
-} else if (process.argv[2] === 'feed') {
-  let feedDir = path.resolve(__dirname, '../', config.feed.outDir);
-  let feedFilename = `${ config.feed.filename }.xml`;
+    case 'feed':
+      const feedDir = path.resolve(__dirname, '../', config.feed.outDir);
+      const feedFilename = `${ config.feed.filename }.xml`;
 
-  shell.rm('-rf', `${ feedDir }/${ feedFilename }`);
-} else {
-  let buildDir = path.resolve(__dirname, '../', config.build.outDir),
-      prerenderOutDir = path.resolve(__dirname, '../', config.prerender.outDir);
+      shell.rm('-rf', `${ feedDir }/${ feedFilename }`);
+      break;
 
-  shell.rm('-rf', buildDir, prerenderOutDir);
+    default:
+      const buildDir = path.resolve(__dirname, '../', config.build.outDir);
+      const prerenderOutDir = path.resolve(__dirname, '../', config.prerender.outDir);
+
+      shell.rm('-rf', buildDir, prerenderOutDir);
 }
